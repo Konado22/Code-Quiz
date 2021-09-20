83a8679f4f2b7f88816152c6e098ff824fa4a579
Jesse Draper <jessedraper2@gmail.com>
2021-09-20 16:54:29 -0600

    add event listener and work on events

--- java.js
+++ java.js
@@ -1,9 +1,20 @@
-var timerSet= 80
-var timer= setInterval(function(){
 
+document.getElementById("start-button").addEventListener("click",function(){
+    var timerSet= 80
+timer= setInterval(function(){
+    console.log("timerSet", timerSet);
     document.getElementById("timer-slot").textContent = timerSet
-    timerSet= timerSet-1;
-    }, 1000)
+    if (timerSet>0){
+        timerSet--;
+    }}, 1000)
+    document.getElementById("start-button").style.display= "none"
+})
+document.getElementById("start-button").addEventListener("click",function (){
+    document.getElementById("question-line").textContent="Commonly used data types do NOT include:";
+    document.getElementById("answer-1").textContent = "Strings";
+    document.getElementById("answer-2").textContent= "Booleans"
+    document.getElementById("answer-3").textContent = "Alerts"; 
+    document.getElementById("answer-4").textContent = "Numbers";
+})
 
-document.getElementById("start-button").addEventListener("click", )
 
