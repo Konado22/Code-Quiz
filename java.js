d87288a81b9c8e2bed7c05c153ae20d236cccd41
Jesse Draper <jessedraper2@gmail.com>
2021-09-20 12:29:37 -0600

    start working on js

---
+++ java.js
@@ -0,0 +1,9 @@
+var timerSet= 80
+var timer= setInterval(function(){
+
+    document.getElementById("timer-slot").textContent = timerSet
+    timerSet= timerSet-1;
+    }, 1000)
+
+document.getElementById("start-button").addEventListener("click", )
+
